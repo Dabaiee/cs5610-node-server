9f2996a344632adff5cf7ddc6d63fc4811dc8d6b
001496270_TongZhou <44611330+Dabaiee@users.noreply.github.com>
2022-12-15 07:27:18 -0800

    cors

--- app.js
+++ app.js
@@ -17,10 +17,27 @@ mongoose.connect(CONNECTION_STRING, (err) => {
 });
 
 const app = express();
+var allowedOrigins = ['http://localhost:3000',
+    'https://strong-sherbet-26589e.netlify.app/'];
+
+// app.use(cors({
+//     credentials: true,
+//     origin: 'http://localhost:3000'
+// }))
 app.use(cors({
     credentials: true,
-    origin: 'http://localhost:3000'
-}))
+    origin: function(origin, callback){
+        // allow requests with no origin
+        // (like mobile apps or curl requests)
+        if(!origin) return callback(null, true);
+        if(allowedOrigins.indexOf(origin) === -1){
+            var msg = 'The CORS policy for this site does not ' +
+                'allow access from the specified Origin.';
+            return callback(new Error(msg), false);
+        }
+        return callback(null, true);
+    }
+}));
 app.use(session({
     secret: 'should be an environment variable',
     resave: false,
